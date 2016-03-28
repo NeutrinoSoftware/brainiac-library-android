3b827fb989f911f89b5c74020708e64e9a9e58b3
Oleg Kasyanov <kasoleg@rambler.ru>
2016-03-28 19:01:06 +0300

    add junit 4 dependency

---
+++ brainiac/src/test/java/net/neutrinosoft/brainiac/BrainiacManagerTest.java
@@ -0,0 +1,80 @@
+package net.neutrinosoft.brainiac;
+
+import android.app.Activity;
+
+import junit.framework.TestCase;
+
+public class BrainiacManagerTest extends TestCase {
+
+    private BrainiacManager brainiacManager;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        brainiacManager = BrainiacManager.getBrainiacManager(new Activity());
+    }
+
+    public void testStartScan() throws Exception {
+
+    }
+
+    public void testIsConnected() throws Exception {
+
+    }
+
+    public void testIsInTestMode() throws Exception {
+
+    }
+
+    public void testOnLeScan() throws Exception {
+
+    }
+
+    public void testStopScan() throws Exception {
+
+    }
+
+    public void testOnConnectionStateChange() throws Exception {
+
+    }
+
+    public void testOnServicesDiscovered() throws Exception {
+
+    }
+
+    public void testOnCharacteristicRead() throws Exception {
+
+    }
+
+    public void testOnCharacteristicChanged() throws Exception {
+
+    }
+
+    public void testGetBatteryLevel() throws Exception {
+
+    }
+
+    public void testRelease() throws Exception {
+
+    }
+
+    public void testStartTest() throws Exception {
+
+    }
+
+    public void testStopTest() throws Exception {
+
+    }
+
+    public void testEnableIndicators() throws Exception {
+
+    }
+
+    public void testDisableIndicators() throws Exception {
+
+    }
+
+    public void testGetIndicatorsState() throws Exception {
+
+    }
+}
