c803e7eeeb84abf4320e437d8e9aa54ee77a1037
Oleg Kasyanov <kasoleg@rambler.ru>
2016-04-04 17:35:25 +0300

    add fabric crashlytics

--- app/src/main/java/net/neutrinosoft/brainactivity/activities/MainActivity.java
+++ app/src/main/java/net/neutrinosoft/brainactivity/activities/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //throw new RuntimeException("Crashlytics test");
+
         PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
 
